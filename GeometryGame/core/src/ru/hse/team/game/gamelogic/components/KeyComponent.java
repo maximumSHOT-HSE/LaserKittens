c67de97f2bd1e0d35fbc5011f99df6d215d68fde
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-06-28 13:37:28 +0300

    add final and fix indentations

--- GeometryGame/core/src/ru/hse/team/game/gamelogic/components/KeyComponent.java
+++ GeometryGame/core/src/ru/hse/team/game/gamelogic/components/KeyComponent.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.utils.Pool.Poolable;
  * Contains door which that object should open on destroy.
  */
 public class KeyComponent implements Component, Poolable {
-
     public Entity door = null;
 
     @Override
--- GeometryGame/core/src/ru/hse/team/game/gamelogic/components/MessageComponent.java
+++ GeometryGame/core/src/ru/hse/team/game/gamelogic/components/MessageComponent.java
@@ -5,14 +5,15 @@ import com.badlogic.gdx.utils.Pool.Poolable;
 
 
 public class MessageComponent implements Component, Poolable {
+    private static final long NEGATIVE_TIME_INFINITY = (long) -1e9;
 
-    public long lastTimeShown = (long)-1e9;
+    public long lastTimeShown = NEGATIVE_TIME_INFINITY;
     public boolean likeToShow = false;
     public String message = "";
 
     @Override
     public void reset() {
-        lastTimeShown = (long)-1e9;
+        lastTimeShown = NEGATIVE_TIME_INFINITY;
         likeToShow = false;
         message = "";
     }
