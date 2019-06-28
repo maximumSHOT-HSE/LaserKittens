537ae35f17b2bbd5742eb2f5db650d00418e30db
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-06-28 13:21:57 +0300

    fix field name and indentations

--- GeometryGame/core/src/ru/hse/team/game/gamelogic/algorithms/DisjointSetUnion.java
+++ GeometryGame/core/src/ru/hse/team/game/gamelogic/algorithms/DisjointSetUnion.java
@@ -1,22 +1,29 @@
 package ru.hse.team.game.gamelogic.algorithms;
 
+/**
+ * Data structure for processing system of
+ * disjoint sets
+ */
 public class DisjointSetUnion {
+    private int verticesNumber;
+    private final int[] parent;
 
-    // max number of vertex, vertices are numbered from 0 to n - 1
-    // at the very beginning there are noe any edge
-    private int n;
-    private int[] parent;
+    /**
+     * Constructs default system of sets each of which
+     * consists of exactly one element
+     *
+     * @param verticesNumber is the number of initial sets called vertices
+     */
+    public DisjointSetUnion(int verticesNumber) {
+        this.verticesNumber = verticesNumber;
+        parent = new int[verticesNumber];
+        for (int i = 0; i < verticesNumber; i++) {
+            parent[i] = i;
+        }
+    }
 
     public int find(int x) {
         return x == parent[x] ? x : (parent[x] = find(parent[x]));
-    }
-
-    public DisjointSetUnion(int n) {
-        this.n = n;
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
     }
 
     public boolean areConnected(int u, int v) {
