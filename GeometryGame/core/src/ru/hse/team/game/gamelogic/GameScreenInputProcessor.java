311adde3797dc74f101c5c07b9d827947f6ce670
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-06-28 13:54:53 +0300

    fix code style

--- GeometryGame/core/src/ru/hse/team/game/gamelogic/GameScreenInputProcessor.java
+++ GeometryGame/core/src/ru/hse/team/game/gamelogic/GameScreenInputProcessor.java
@@ -32,7 +32,6 @@ import ru.hse.team.game.levels.AbstractLevel;
  * Game status timer starts on first touch or movement with accelerometer
  */
 public class GameScreenInputProcessor implements InputProcessor {
-
     private final LaserKittens laserKittens;
     private final Entity focusedPlayer;
     private final AbstractLevel abstractLevel;
@@ -58,7 +57,11 @@ public class GameScreenInputProcessor implements InputProcessor {
         return dragging;
     }
 
-    public GameScreenInputProcessor(LaserKittens laserKittens, AbstractLevel abstractLevel, OrthographicCamera camera) {
+    public GameScreenInputProcessor(
+        LaserKittens laserKittens,
+        AbstractLevel abstractLevel,
+        OrthographicCamera camera
+    ) {
         this.laserKittens = laserKittens;
         this.focusedPlayer = abstractLevel.getPlayer();
         this.abstractLevel = abstractLevel;
@@ -67,12 +70,16 @@ public class GameScreenInputProcessor implements InputProcessor {
         enabledAccelerometer = laserKittens.getPreferences().isEnabledAccelerometer();
 
         ground = BodyFactory.getBodyFactory(abstractLevel.getWorld())
-                .newCircleBody(new Vector2(0, abstractLevel.getLevelHeightInScreens() *
-                    RenderingSystem.getScreenSizeInMeters().y * 2),
+            .newCircleBody(
+                new Vector2(
+                    0,
+                    abstractLevel.getLevelHeightInScreens() *
+                            RenderingSystem.getScreenSizeInMeters().y * 2
+                ),
                 0.1f,
                 BodyDef.BodyType.StaticBody,
                 true
-        );
+           );
     }
 
     @Override
@@ -113,7 +120,6 @@ public class GameScreenInputProcessor implements InputProcessor {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        
         camera.unproject(position.set(screenX, screenY, 0));
         abstractLevel.getGameStatus().start();
 
@@ -124,7 +130,8 @@ public class GameScreenInputProcessor implements InputProcessor {
             } else {
                 abstractLevel.shoot(position.x, position.y);
             }
-            Sound laser = laserKittens.getAssetManager().getSound(KittensAssetManager.Sounds.LASER_SOUND);
+            Sound laser = laserKittens.getAssetManager()
+                    .getSound(KittensAssetManager.Sounds.LASER_SOUND);
             laser.play(laserKittens.getPreferences().getSoundVolume());
             return true;
         }
@@ -159,7 +166,6 @@ public class GameScreenInputProcessor implements InputProcessor {
             mouseJoint = (MouseJoint) abstractLevel.getWorld().createJoint(def);
         });
         playerBody.setAwake(true);
-
 
         return true;
     }
@@ -197,22 +203,22 @@ public class GameScreenInputProcessor implements InputProcessor {
     }
 
     public void touchDraggedExplicitly() {
-
         if (enabledAccelerometer) return;
-
         if (draggingPointer != -1) {
             int screenX = Gdx.input.getX(draggingPointer);
             int screenY = Gdx.input.getY(draggingPointer);
-
             touchDragged(screenX, screenY, draggingPointer);
         }
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-
-        if (!dragging || pointer != draggingPointer) return false;
-        if (enabledAccelerometer) return false;
+        if (!dragging || pointer != draggingPointer) {
+            return false;
+        }
+        if (enabledAccelerometer) {
+            return false;
+        }
 
         camera.unproject(draggingPosition.set(screenX, screenY, 0));
         draggingPosition.x -= draggingStartedDiff.x;
@@ -227,7 +233,9 @@ public class GameScreenInputProcessor implements InputProcessor {
     }
 
     public void moveWithAccelerometer(float delta) {
-        if (!enabledAccelerometer) return;
+        if (!enabledAccelerometer) {
+            return;
+        }
 
         abstractLevel.getGameStatus().start();
 
@@ -241,17 +249,25 @@ public class GameScreenInputProcessor implements InputProcessor {
 
         if (Math.abs(accelerometerX) < 0.1 && Math.abs(accelerometerY) < 0.1) {
             playerBody.applyForce(
-                    -playerBody.getLinearVelocity().x * delta * 1e3f * playerBody.getMass(),
-                    -playerBody.getLinearVelocity().y * delta * 1e3f * playerBody.getMass(),
-                    playerBody.getPosition().x, playerBody.getPosition().y, true);
+                -playerBody.getLinearVelocity().x * delta * 1e3f * playerBody.getMass(),
+                -playerBody.getLinearVelocity().y * delta * 1e3f * playerBody.getMass(),
+                playerBody.getPosition().x,
+                playerBody.getPosition().y,
+                true
+            );
             return;
         }
 
         float forceX = Math.max(2, Math.abs(accelerometerX)) * delta * 1e3f * playerBody.getMass();
         float forceY = Math.max(2, Math.abs(accelerometerY)) * delta * 1e3f * playerBody.getMass();
 
-        playerBody.applyForce(-Math.signum(accelerometerX) * forceX, -Math.signum(accelerometerY) * forceY                                                                      ,
-                accelerometerX + playerBody.getPosition().x, accelerometerY + playerBody.getPosition().y, true);
+        playerBody.applyForce(
+            -Math.signum(accelerometerX) * forceX,
+            -Math.signum(accelerometerY) * forceY,
+            accelerometerX + playerBody.getPosition().x,
+            accelerometerY + playerBody.getPosition().y,
+            true
+        );
     }
 
     @Override
